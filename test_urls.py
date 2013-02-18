aab9d20c07125202ab56906308f307bb503586b5
Yohann Gabory <yohann@gabory.fr>
2013-02-18 20:07:57 +0100

    removed the tests for django 1.3

--- test_urls.py
+++ test_urls.py
@@ -1,8 +1,4 @@
-from django import VERSION
-if VERSION[1] < 4:
-    from django.conf.urls.defaults import patterns, include, url 
-else:
-    from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
